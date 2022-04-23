b7cfade2a57f99132e2e87a63f7b73ef2364f7ed
Simon Hryszko <simon@hryszko.dev>
2022-04-23 17:28:08 +0200

    Added question validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,9 @@
 class Question < ApplicationRecord
+	validates :question, presence: true
+	validates :ansA, presence: true
+	validates :ansB, presence: true
+	validates :ansC, presence: true
+	validates :ansD, presence: true
+	validates :points, presence: true
+	validates :time, presence: true
 end
