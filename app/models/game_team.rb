80ccb4278b89e02baeb39df7ee7a2330c5cad65c
Simon Hryszko <simon@hryszko.dev>
2022-04-27 10:38:23 +0200

    Revert "Added ref game <-> team (n:n:)"
    This reverts commit e11ead3f826595938aa6b2ba28973007d7b03db0.

--- app/models/game_team.rb
+++
@@ -1,4 +0,0 @@
-class GameTeam < ApplicationRecord
-  belongs_to :game
-  belongs_to :team
-end
--- test/models/game_team_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class GameTeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
