2be36c824a0d139514098902f36fe172a694979d
Łukasz Fida <fidek1999@gmail.com>
2022-04-29 15:28:44 +0200

    translation for toast and actions in games

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -25,7 +25,7 @@ class TeamsController < ApplicationController
 
     respond_to do |format|
       if @team.save
-        format.html { redirect_to actions: "index", notice: "Team was successfully created." }
+        format.html { redirect_to actions: "index", notice: t('succed.team.create') }
         format.json { render :show, status: :created, location: @team }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class TeamsController < ApplicationController
   def update
     respond_to do |format|
       if @team.update(team_params)
-        format.html { redirect_to actions: "index", notice: "Team was successfully updated." }
+        format.html { redirect_to actions: "index", notice: t('succed.team.update') }
         format.json { render :show, status: :ok, location: @team }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class TeamsController < ApplicationController
     @team.destroy
 
     respond_to do |format|
-      format.html { redirect_to teams_url, notice: "Team was successfully destroyed." }
+      format.html { redirect_to teams_url, notice: t('succed.team.delete') }
       format.json { head :no_content }
     end
   end
