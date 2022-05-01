071002284cf35054e1e5b9ebc3d606bd92543eab
Simon Hryszko <simon@hryszko.dev>
2022-05-01 20:08:37 +0200

    Added global method to check if string is img uri

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	def uri_i?(string)
+		uri = URI.parse(string)
+		%w( http https ).include?(uri.scheme)
+		# check if uri has jpg, png, gif, jpeg, bmp, tif, tiff, ico, svg, wbmp, ico, or gif via regex
+		if string =~ /\.(jpg|png|gif|jpeg|bmp|tif|tiff|ico|svg|wbmp|ico|gif)$/
+			return true
+		else
+			return false
+		end
+	rescue URI::BadURIError
+		false
+	rescue URI::InvalidURIError
+		false
+	end
 end
