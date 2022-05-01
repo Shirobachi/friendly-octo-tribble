a4caf20b4c4273d3d980051d04b79a851b4930a9
Simon Hryszko <simon@hryszko.dev>
2022-05-01 23:32:03 +0200

    close #9

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -2,4 +2,36 @@ class MenuController < ApplicationController
   def index
 		
   end
+
+	def misc
+	end
+	
+	def deactivate
+		questions = Question.where(
+			used: params[:used]..,
+			active: true
+		)
+
+		redirect_to menu_misc_path, notice: "Deactivated #{questions.count} questions."
+
+		questions.each do |q|
+			q.active = false
+			q.save
+		end
+	end
+	
+	def activate
+		questions = Question.where(
+			used: ..params[:used],
+			active: false
+		)
+		c = questions.count
+
+		questions.each do |q|
+			q.active = true
+			q.save
+		end
+
+		redirect_to menu_misc_path, notice: "Activated #{c} questions."
+	end
 end
