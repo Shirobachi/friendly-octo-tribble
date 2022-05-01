5c5a2204f970128f972ce73f6d891d289567fa34
Simon Hryszko <simon@hryszko.dev>
2022-05-01 21:20:04 +0200

    Reverse part of 4d26369eb0bb8f51dbe9b33e63e2cc9738cc3d55

--- db/migrate/20220501172522_rename_question_url_to_justification_url.rb
+++ db/migrate/20220501172522_rename_question_url_to_justification_url.rb
@@ -1,6 +1,6 @@
 class RenamejustificationUrlToJustificationUrl < ActiveRecord::Migration[7.0]
   def change
 		# Ranme column
-		rename_column :questions, :justificationUrl, :justificationUrl
+		rename_column :questions, :questionUrl, :justificationUrl
   end
 end
