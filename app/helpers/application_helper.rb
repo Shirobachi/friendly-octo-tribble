bb29176f3726dae68b45bebf00e601c0a3713b14
Simon Hryszko <simon@hryszko.dev>
2022-05-04 14:11:03 +0200

    Improve uri_i? def and add uri?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,17 @@
 module ApplicationHelper
-	def uri_i?(string)
-		uri = URI.parse(string)
-		%w( http https ).include?(uri.scheme)
-		# check if uri has jpg, png, gif, jpeg, bmp, tif, tiff, ico, svg, wbmp, ico, or gif via regex
-		if string =~ /\.(jpg|png|gif|jpeg|bmp|tif|tiff|ico|svg|wbmp|ico|gif)$/
+	def uri?(url)
+		if url =~ URI::regexp
 			return true
 		else
 			return false
 		end
-	rescue URI::BadURIError
-		false
-	rescue URI::InvalidURIError
-		false
+	end
+
+	def uri_i?(string)
+		if uri?(string) && string =~ /\.(jpg|png|gif|jpeg|bmp|tif|tiff|ico|svg|wbmp|ico|gif)$/
+			return true
+		else
+			return false
+		end
 	end
 end
